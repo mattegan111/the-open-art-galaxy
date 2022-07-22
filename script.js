404246aea077f8bae5249b159bcbd2f97e2b0416
Matthew Egan <mnegan93@gmail.com>
2022-07-22 23:32:52 +1200

    Add lights, 50 'planets' and render

--- script.js
+++ script.js
@@ -14,4 +14,39 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20, 20, 20);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, .1);
+
+scene.add(pointLight, ambientLight);
+
+// Not yet implemented
+// const controls = new OrbitControls(camera, renderer.domElement)
+
+const geometryStar = new THREE.SphereGeometry((Math.random()), 24, 24);
+const materialStar = new THREE.MeshStandardMaterial({color: 0xffffff});
+
+function addStar(){
+    const meshStar = new THREE.Mesh(geometryStar, materialStar);
+
+    const [x, y, z] = Array(3)
+        .fill()
+        .map(() => THREE.MathUtils.randFloatSpread(50));
+
+    meshStar.position.set(x, y, z);
+    scene.add(meshStar);
+}
+
+Array(50).fill().forEach(addStar);
+
+//Not yet implemented
+// function animate() {
+//   requestAnimationFrame(animate);
+//   controls.update();
+//   renderer.render(scene, camera);
+// }
+
+// animate();
+
 renderer.render(scene, camera);
