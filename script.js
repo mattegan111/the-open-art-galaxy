20ce817d26416c46114484b457ba2b6035cfbd7a
Matthew Egan <mnegan93@gmail.com>
2022-07-22 21:06:21 +1200

    Add empty 3D scene

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+// Not yet implemented:
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
