ff805d510f5285a2a40d2ff4b4d851d694543d07
Matthew Egan <mnegan93@gmail.com>
2022-07-25 23:47:24 +1200

    Implement Orbit Controls using new cdn link

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
-// Not yet implemented:
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 
@@ -14,6 +13,8 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
+renderer.render(scene, camera);
+
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(20, 20, 20);
 
@@ -21,32 +22,28 @@ const ambientLight = new THREE.AmbientLight(0xffffff, .1);
 
 scene.add(pointLight, ambientLight);
 
-// Not yet implemented
-// const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const geometryStar = new THREE.SphereGeometry((Math.random()), 24, 24);
 const materialStar = new THREE.MeshStandardMaterial({color: 0xffffff});
 
 function addStar(){
     const meshStar = new THREE.Mesh(geometryStar, materialStar);
-
+    
     const [x, y, z] = Array(3)
-        .fill()
-        .map(() => THREE.MathUtils.randFloatSpread(50));
-
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(50));
+    
     meshStar.position.set(x, y, z);
     scene.add(meshStar);
 }
 
 Array(50).fill().forEach(addStar);
 
-//Not yet implemented
-// function animate() {
-//   requestAnimationFrame(animate);
-//   controls.update();
-//   renderer.render(scene, camera);
-// }
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
 
-// animate();
-
-renderer.render(scene, camera);
+animate();
