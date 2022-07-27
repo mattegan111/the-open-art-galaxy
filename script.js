fcc2827253b0c616fde01b2b1ed3bbef1880c0a4
Matthew Egan <mnegan93@gmail.com>
2022-07-27 17:46:37 +1200

    Add background for demonstration purposes

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.m
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
 
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
